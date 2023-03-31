535f9ae8eeadb8441fd0265ee721f568c0543a29
Juho Simojoki <juho.simojoki@metropolia.fi>
2023-03-31 16:47:07 +0300

    Created a model for the blog.

---
+++ models/blogs.js
@@ -0,0 +1,29 @@
+const { Sequelize, Model, DataTypes, BelongsTo } = require('sequelize')
+
+class Blog extends Model {}
+
+Blog.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    author: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
+    title: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    url: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    likes: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+    }
+})
+
+module.exports = { Blog }
